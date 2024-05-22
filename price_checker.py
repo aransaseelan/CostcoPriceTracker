d23d85b792e4c0d0de6d44cc9d0e834e54dd49ba
Aran <aran.saseelan@gmail.com>
2024-05-22 02:25:02 -0400

    Worked on refactoring the code, so that there were less unnecessary classes. Having single resposibility is good but if over used can be over kill.

--- price_checker.py
+++ price_checker.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,30 +6,41 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import requests
 import time
+from selenium import webdriver
 
+# Checklist for products: Name, Price, Image, URL
 def getCurrentPrice(url):
     
     driver = webdriver.Firefox()
+    time.sleep(12)
     
-    try:
-        # Load the webpage
-        driver.get(url)
+    # Get the page source and pass it to BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    
+    # Find the price element
+    price = get_price(soup)
+    image = get_image(soup)
+    name = get_name(soup)
 
-        # Wait for additional seconds if necessary to ensure all content is loaded
-        time.sleep(15)
+    return price, image, name  
+    
+def get_price(soup):
+    price_element = soup.find('span', attrs={'automation-id': 'productPriceOutput'})
+    if price_element is not None:
+        return price_element.text
+    else:
+        return None
 
-        # Get the page source and pass it to BeautifulSoup
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        
+def get_image(soup):
+    image_element = soup.find('img', attrs={'automation-id': 'initialProductImage'})
+    if image_element is not None:
+        return image_element['src']
+    else:
+        return None
 
-        # Find the price element
-        price_element = soup.find('span', attrs={'automation-id': 'productPriceOutput'})
-        if price_element is not None:
-            currentPrice = price_element.text
-            print(currentPrice)
-        else:
-            print("Price element not found.")
-
-    finally:
-        # Close the WebDriver
-        driver.quit()
+def get_name(soup):
+    name_element = soup.find('h1', attrs={'automation-id': 'productName'})
+    if name_element is not None:
+        return name_element.text
+    else:
+        return None
