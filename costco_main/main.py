543bbdae34a87ffc22cd03d99db1520b26cdc57f
aransaseelan <aran.saseelan@gmail.com>
2025-03-08 23:37:43 -0500

    changes to file.

--- costco_main/main.py
+++ costco_main/main.py
@@ -29,9 +29,10 @@ def main():
 
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
     options.add_argument('--ignore-certificate-errors-spki-list')
     options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
     driver = webdriver.Chrome(options=options)
